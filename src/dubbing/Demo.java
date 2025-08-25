04fecffd9a0a9f5a1e2b1c51ca9783fa47b52e9b
akhileshwagh <akhimech05@gmail.com>
2025-08-25 20:39:25 +0530

    dubbing added

---
+++ src/dubbing/Demo.java
@@ -0,0 +1,23 @@
+package dubbing;
+
+public class Demo {
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 6;
+
+		int sum = a + b;
+
+		System.out.println("sum = " + sum);
+		
+		int sub=sum-7;
+		
+		System.out.println("sub = " + sub);
+		int mul=sub*4;
+		
+		System.out.println("mul = " + mul);
+		int divide=mul/0;
+		
+		System.out.println("divide = " + divide);
+	}
+}
